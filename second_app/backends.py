acc0d58bc7bb20b65d13c9b2a02c0e996fd36a3e
amirsina mandegari <mandegari.amirsina@gmail.com>
2025-05-23 11:33:23 +0330

    use model backend for custom backend to works with admin

--- second_app/backends.py
+++ second_app/backends.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class EmailBackend(BaseBackend):
+class EmailBackend(ModelBackend):
     """Authenticate using email instead of username"""
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None or password is None:
