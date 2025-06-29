088eba97233480ba2065d58494dc790621852b93
amirsina mandegari <mandegari.amirsina@gmail.com>
2025-06-29 19:31:07 +0330

    use custom serilizer for m2m and validations

--- first_drf_app/serializers.py
+++ first_drf_app/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
-from first_drf_app.models import Company
+from first_drf_app.models import Company, Employee
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('first_name', 'last_name')
 
 
 class CompanySerializer(serializers.ModelSerializer):
     age_in_days = serializers.SerializerMethodField()
     age_in_month = serializers.IntegerField(read_only=True)
+    employees = EmployeeSerializer(many=True)
     
     class Meta:
         model = Company
@@ -18,6 +25,20 @@ class CreateCompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         exclude = ('id', 'employees')
+
+    def validate_name(self, value):
+        if 'company' in value:
+            raise serializers.ValidationError('your name should not have company')
+        return value
+    
+    def validate(self, attrs):
+        name = attrs.get('name')
+        manager_name = attrs.get('manager_name')
+        if name == manager_name:
+            raise serializers.ValidationError('name and manager name should not be same!')
+        return super().validate(attrs)
+
+
 
 
 class CompanyEmailSerializer(serializers.ModelSerializer):
