238e49edd4531a64b34f5b2e14acb8a10bd518f3
amirsina mandegari <mandegari.amirsina@gmail.com>
2025-07-04 10:18:06 +0330

    add swagger with drf-yasg to the project

--- maktab_django_126/urls.py
+++ maktab_django_126/urls.py
@@ -20,10 +20,26 @@ from django.contrib.auth import views as auth_view
 from first_app.urls import urlpatterns as first_app_urls
 from second_app.urls import urlpatterns as second_app_urls 
 from first_drf_app.urls import urlpartterns as first_drf_app_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="maktab django 126",
+        default_version='v1',
+        description="our very first drf swagger on maktab django 126"
+    ),
+    public=True,
+    permission_classes=[AllowAny]
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first_app/', include(first_app_urls)),
     path('second_app/', include(second_app_urls)),
     path('first_drf_app/', include(first_drf_app_urls)),
-    path('login/', auth_view.LoginView.as_view())
+    path('login/', auth_view.LoginView.as_view()),
+    path('swagger/', schema_view.with_ui(), name='schema-swagger-ui')
 ]
