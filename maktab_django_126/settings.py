c16fe7f7369c2d97053f8968099ae344c581a8b5
amirsina mandegari <mandegari.amirsina@gmail.com>
2025-08-01 08:51:05 +0330

    setup redis cache for django

--- maktab_django_126/settings.py
+++ maktab_django_126/settings.py
@@ -88,6 +88,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 REST_FRAMEWORK = {
     'SEARCH_PARAM': 'q'
 }
